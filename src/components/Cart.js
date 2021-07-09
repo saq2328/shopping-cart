38c39de2604656a22c583f6820a92ddec85f7b61
saq2328 <msgamer.khan@gmail.com>
2021-07-09 13:51:25 -0700

    Dont Know

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -25,7 +25,7 @@ export default class Cart extends Component {
                                 </button>
                             </div>     
                         </li>
-                        ))};
+                        ))}
                     </ul>
                 </div>
             </div>    
