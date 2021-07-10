21ddbbeaef791116c595c478cb534e34418413a6
saq2328 <msgamer.khan@gmail.com>
2021-07-10 14:17:51 -0700

    update2

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import formatCurrency from "../util";
+import Fade from "react-reveal/Fade";
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
 
 
 export default class Products extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            product: null
+        };
+    }
+    openModal = (product) => {
+        this.setState({ product: null });
+    }
+    closeModal = () =>{
+        this.setState({product: null})
+    }
     render() {
+        const {product} = this.state;
         return (
             <div>
+                <Fade bottom cascade>
                 <ul className="products">
                     {this.props.products.map((product) =>(
                         <li key={product._id}>
                             <div className="product">
-                                <a href={"#" + product._id}>
+                                <a href={"#" + product._id} onClick={() => this.openModal(product)}>
                                     <img src={product.image} alt={product.title}></img>
                                     <p>
                                         {product.title}
@@ -28,7 +45,17 @@ export default class Products extends Component {
                         </li>
                     ))}
                 </ul>
+                </Fade>
+                {product && (
+                  <Modal isOpen = {true}
+                  onRequestClose={this.closeModal}>
+                    <Zoom>
+                        <button className="close-modal" onClick={this.closeModal}>x</button>
+                        <div>Modal</div>
+                    </Zoom>
+                  </Modal>)
+                }
             </div>
-        )
+        );
     }
 }
